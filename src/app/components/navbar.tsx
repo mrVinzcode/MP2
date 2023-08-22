ceb7cfedeb2bf8bfa94bfc313e7d28a90102dc5c
Melvin Mangubat <137170998+mrVinzcode@users.noreply.github.com>
2023-08-22 21:32:39 +0800

    add semantic element header to navbar component

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,28 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="container pt-6">
-      <div className="flex justify-between items-center">
-        <Image src="/logo.png" height={50} width={50} alt="logo" />
-        <ul className="md:flex hidden gap-8 items-center font-semibold text-[14px]">
-          <li>Home</li>
-          <li>About</li>
-          <li>Food</li>
-          <li>Dish</li>
-          <li>Contact</li>
+    <header className="relative">
+      <div className="container pt-6 fixed top-0 left-0 right-0">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <Image src="/logo.png" height={50} width={50} alt="logo" />
+          </Link>
+          <ul className="md:flex hidden gap-8 items-center font-semibold text-[14px]">
+            <li>Home</li>
+            <li>About</li>
+            <li>Food</li>
+            <li>Dish</li>
+            <li>Contact</li>
 
-          <button className="semi-bold bg-accent text-white px-6 rounded-3xl py-2 ">
-            SignUp
-          </button>
-        </ul>
+            <button className="semi-bold bg-accent text-white px-6 rounded-3xl py-2 ">
+              SignUp
+            </button>
+          </ul>
 
-        <AiOutlineMenu className="md:hidden text-accent" size={30} />
+          <AiOutlineMenu className="md:hidden text-accent" size={30} />
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
