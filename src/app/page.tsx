1e73de2a1f50898be608037eb5b77316bf00f382
dyuwani <johoneybernardino@gmail.com>
2023-08-22 18:18:43 +0800

    Adding initial files.

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Hero from "./components/hero";
+import Feature from "./components/Feature";
+import Services from "./components/Services";
+import Menu from "./components/Menu";
+import Survey from "./components/Survey";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Feature />
+      <Services />
+      <Menu />
+      <Survey />
+    </main>
+  );
+}
