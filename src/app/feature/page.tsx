d546b6e78ae276349abafbb6f14af2e32aabf5ce
Melvin Mangubat <137170998+mrVinzcode@users.noreply.github.com>
2023-08-23 17:40:04 +0800

    add togglemenu open and close for mobile setup

--- src/app/feature/page.tsx
+++ src/app/feature/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const AboutPage = () => {
+const FeaturePage = () => {
   return <div>No About page pa</div>;
 };
 
-export default AboutPage;
+export default FeaturePage;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <header className="relative">
-      <div className="container pt-6 fixed top-0 left-0 right-0">
+      <nav className="container pt-6 fixed top-0 left-0 right-0">
         <div className="flex justify-between items-center">
           {/*Desktop View */}
           <Link href="/">
@@ -26,7 +31,7 @@ const Navbar = () => {
             </li>
             <li className="cursor-pointer  text-md uppercase border-white hover:text-accent">
               {/* <a> */}
-              <Link href="/about">Featured</Link>
+              <Link href="/feature">Featured</Link>
               {/* </a> */}
             </li>
             <li className="cursor-pointer text-md uppercase border-white hover:text-accent">
@@ -49,9 +54,68 @@ const Navbar = () => {
             </Link>
           </ul>
           {/* Mobile Menu */}
-          <AiOutlineMenu className="md:hidden text-accent" size={30} />
+
+          <AiOutlineMenu
+            onClick={toggleMenu}
+            className="text-accent md:hidden mb-10 cursor-pointer"
+            size={30}
+          />
+          <div
+            className={
+              menuOpen
+                ? "fixed top-0 left-0 w-[-75%] md:hidden h-screen p-10 ease-in-out duration-500 bg-slate-800"
+                : "fixed left-[-100%] top-0 p-10 ease-in-out duration-500"
+            }
+          >
+            <div className="flex w-full items-center justify-end">
+              <div onClick={toggleMenu} className="cursor-pointer">
+                <AiOutlineClose className="text-accent" size={30} />
+              </div>
+            </div>
+            {/* Mobile Menu Links*/}
+            <ul>
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-2 hover:bg-accent text-center p-10 my-2"
+              >
+                <Link href="/">Home</Link>
+              </li>
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-2 hover:bg-accent text-center p-10 my-2"
+              >
+                <Link href="/feature">Feature</Link>
+              </li>
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-2 hover:bg-accent text-center p-10 my-2"
+              >
+                <Link href="/servicespage">Services</Link>
+              </li>
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-2 hover:bg-accent text-center p-10 my-2"
+              >
+                <Link href="/menu">Menu</Link>
+              </li>
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-2 hover:bg-accent text-center p-10 my-2"
+              >
+                <Link href="/">Contact</Link>
+              </li>
+              <Link href="/signup">
+                <button
+                  onClick={() => setMenuOpen(false)}
+                  className="my-2 p-10 text-center cursor-pointer semi-bold bg-accent text-white rounded-3xl py-2 hover:bg-accent hover:text-black duration-150 ease-in-out "
+                >
+                  SignUp
+                </button>
+              </Link>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
