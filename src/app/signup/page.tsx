cf699021b3bd047a0e85267000699e681acc633d
dyuwani <johoneybernardino@gmail.com>
2023-09-02 16:59:53 +0800

    Updated Buttons and Signup Page >> >> Changed button words to Reseserve now for Reservation and Contact Us Page >> >> Updated reservation page, added Date and Time of Reservation, Changed name to Taste of Korea

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,7 +6,7 @@ const SignupPage = () => {
       <div className="container px-5 py-24 mx-auto flex flex-wrap items-center pr-4">
         <div className="lg:w-1/3 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
           <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
-            Sign Up
+            Reserve Now! See you soon.
           </h2>
           <div className="mb-4">
             <label
@@ -34,9 +34,35 @@ const SignupPage = () => {
               name="email"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
+            </div>
+            <div className="mb-4">
+             <label
+              htmlFor="email"
+              className="leading-7 text-sm text-gray-600"
+            />
+            Date of Reservation
+            <input
+              type="date"
+              id="dateOfReservation"
+              name="date"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+            </div>
+            <div className="mb-4">
+             <label
+              htmlFor="email"
+              className="leading-7 text-sm text-gray-600"
+            />
+            Time of Reservation
+            <input
+              type="time"
+              id="timeOfReservation"
+              name="time"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
           </div>
-          <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Button
+          <button className="my-2 p-10 text-center cursor-pointer semi-bold bg-accent text-white rounded-3xl py-2 hover:bg-accent hover:text-black duration-150 ease-in-out ">
+            Reserve Now
           </button>
           <p className="text-xs text-gray-500 mt-3"></p>
         </div>
@@ -45,7 +71,7 @@ const SignupPage = () => {
             🥢 Welcome to Taste of Seoul! 🥢
           </h1>
           <p className="leading-relaxed mt-4 max-w-[500px] text-center">
-            🌟 Why Join Taste of Seoul? 🌟 <br /> Explore Authentic Recipes:
+            🌟 Why Taste of Korea? 🌟 <br /> Explore Authentic Recipes:
             Discover traditional and modern Korean recipes curated by our expert
             chefs. 📸 <br />
             Foodie Community: Share your own culinary creations, tips, and
