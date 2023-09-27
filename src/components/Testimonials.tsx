1f94fd72b2870a110dda30c67e115f6dcb29abe5
dyuwani <johoneybernardino@gmail.com>
2023-09-28 03:15:16 +0800

    Updated Contact us and Testimonials Added Thank you Message after submitting the form Replaced wordings for testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -17,22 +17,10 @@ const Testimonials = () => {
           <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
             <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Speechless with how easy this was to integrate
+              VERY GOOD KOREAN FOOD!!!
               </h3>
               <p className="my-4">
-                &quot;I recently got my hands on Flowbite Pro, and holy crap,
-                I&rsquo;m speechless with how easy this was to integrate within
-                my application. Most templates are a pain, code is scattered,
-                and near impossible to theme.
-              </p>
-              <p className="my-4">
-                Flowbite has code in one place and I&rsquo;m not joking when I
-                say it took me a matter of minutes to copy the code, customise
-                it and integrate within a Laravel + Vue application.
-              </p>
-              <p className="my-4">
-                If you care for your time, I hands down would go with
-                this.&quot;
+              &quot;Absolutely loved my dining experience at Taste of Seoul! The ambiance was simply delightful; from the warm and welcoming decor to the soothing Korean music playing in the background, it truly transported me to Seoul. The service was top-notch; the staff was attentive and friendly, making sure we had everything we needed. And the food? Oh, it was an explosion of flavors! Every dish was a culinary masterpiece, and I can't wait to come back for more.
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
@@ -46,7 +34,7 @@ const Testimonials = () => {
               <div className="space-y-0.5 font-medium dark:text-white text-left">
                 <div>Bonnie Green</div>
                 <div className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Developer at Open AI
+                  Food Blogger
                 </div>
               </div>
             </figcaption>
@@ -54,18 +42,10 @@ const Testimonials = () => {
           <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 dark:bg-gray-800 dark:border-gray-700">
             <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Solid foundation for any project
+                Fantastic Combination
               </h3>
               <p className="my-4">
-                &quot;FlowBite provides a robust set of design tokens and
-                components based on the popular Tailwind CSS framework. From the
-                most used UI components like forms and navigation bars to the
-                whole app screens designed both for desktop and mobile, this UI
-                kit provides a solid foundation for any project.
-              </p>
-              <p className="my-4">
-                Designing with Figma components that can be easily translated to
-                the utility classes of Tailwind CSS is a huge timesaver!&quot;
+                &quot;I recently dined at Taste of Seoul, and I was blown away by the fantastic combination of great ambiance, excellent service, and delicious food. The restaurant's cozy atmosphere and traditional Korean decor instantly put me at ease. The service staff were knowledgeable about the menu and offered fantastic recommendations. As for the food, it was like a taste journey through Korea. Every bite was a burst of authentic flavors, and I left with a happy heart and a full belly.&quot;
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
@@ -79,7 +59,7 @@ const Testimonials = () => {
               <div className="space-y-0.5 font-medium dark:text-white text-left">
                 <div>Roberta Casas</div>
                 <div className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Lead designer at Dropbox
+                  Influencer
                 </div>
               </div>
             </figcaption>
@@ -87,20 +67,10 @@ const Testimonials = () => {
           <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">
             <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Mindblowing workflow and variants
+                Exceeded Expectations 
               </h3>
               <p className="my-4">
-                &quot;As someone who mainly designs in the browser, I&rsquo;ve
-                been a casual user of Figma, but as soon as I saw and started
-                playing with FlowBite my mind was.
-              </p>
-              <p className="my-4">
-                Everything is so well structured and simple to use (I&rsquo;ve
-                learnt so much about Figma by just using the toolkit).
-              </p>
-              <p className="my-4">
-                Aesthetically, the well designed components are beautiful and
-                will undoubtedly level up your next application.&quot;
+                &quot;Taste of Seoul exceeded all my expectations! The ambiance was spot on, providing a relaxing and authentic Korean dining experience. The service was impeccable; the staff was courteous and went out of their way to make our evening special. And the food was simply divine! Each dish was a masterpiece, and I could taste the passion and dedication that went into preparing them. This restaurant is a hidden gem, and I can't wait to share this fantastic find with my friends and family.&quot;
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
@@ -114,7 +84,7 @@ const Testimonials = () => {
               <div className="space-y-0.5 font-medium dark:text-white text-left">
                 <div>Jese Leos</div>
                 <div className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Software Engineer at Facebook
+                  Travel Blogger
                 </div>
               </div>
             </figcaption>
@@ -122,16 +92,10 @@ const Testimonials = () => {
           <figure className="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12 dark:bg-gray-800 dark:border-gray-700">
             <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Efficient Collaborating
+                Pure Pleasure
               </h3>
               <p className="my-4">
-                &quot;This is a very complex and beautiful set of elements.
-                Under the hood it comes with the best things from 2 different
-                worlds: Figma and Tailwind.
-              </p>
-              <p className="my-4">
-                You have many examples that can be used to create a fast
-                prototype for your team.&quot;
+                &quot;I had the pleasure of dining at Taste of Seoul, and I can't stop raving about it! The ambiance was charming, with a perfect blend of modern and traditional Korean elements. The service was impeccable; the staff was friendly and attentive without being intrusive. And the food...oh, the food was a revelation! Every dish was bursting with flavor, and the presentation was a work of art. I'm so glad I found this gem of a restaurant, and I'll definitely be returning soon for another delightful dining experience.&quot;
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
@@ -150,14 +114,6 @@ const Testimonials = () => {
               </div>
             </figcaption>
           </figure>
-        </div>
-        <div className="text-center">
-          <a
-            href="#"
-            className="my-2 p-10 text-center cursor-pointer semi-bold bg-accent text-white rounded-3xl py-2 px-5 hover:bg-accent hover:text-black duration-150 ease-in-out"
-          >
-            Show more...
-          </a>
         </div>
       </div>
     </section>
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,79 +1,122 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    isSubmitted: false, 
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({ ...formData, isSubmitted: true });
+  };
+
   return (
     <section className="text-gray-600 body-font" id="contact">
       <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col text-center w-full mb-12 ">
+        <div className="flex flex-col text-center w-full mb-12">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
             Contact Us
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base ">
-            &quot;Crafting Memories, One Dish at a Time&quot;
+            "Crafting Memories, One Dish at a Time"
           </p>
         </div>
         <div className="lg:w-1/2 md:w-2/3 mx-auto">
-          <div className="flex flex-wrap -m-2">
-            <div className="p-2 w-1/2">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-            </div>
-            <div className="p-2 w-1/2 ">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-            </div>
-            <div className="p-2 w-full ">
-              <div>
-                <label
-                  htmlFor="message"
-                  className="leading-7 text-sm text-gray-600"
-                >
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-              </div>
-            </div>
-            <div className="p-2 w-full">
-              <button className="flex mx-auto text-white bg-accent border-0 rounded-3xl py-2 px-8 focus:outline-none hover:text-black text-lg">
-                Submit
-              </button>
-            </div>
-            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center ">
-              <a className="text-accent">koreanizefoodapp@email.com</a>
-              <p className="leading-normal my-5">
-                49 Smith St.
-                <br />
-                Saint Cloud, MN 56301
+          {formData.isSubmitted ? (
+            <div className="text-center">
+              <h2 className="text-2xl font-medium mb-4 text-accent">
+                Thank you for contacting us!
+              </h2>
+              <p className="text-lg mb-8">
+                We will get back to you as soon as possible.
               </p>
             </div>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              {/* ... Form input fields here */}
+              <div className="p-2 w-full">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    onChange={handleChange}
+                    value={formData.name}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    onChange={handleChange}
+                    value={formData.email}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div>
+                  <label
+                    htmlFor="message"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    onChange={handleChange}
+                    value={formData.message}
+                    required
+                  ></textarea>
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button
+                  type="submit"
+                  className="flex mx-auto text-white bg-accent border-0 rounded-3xl py-2 px-8 focus:outline-none hover:text-black text-lg"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          )}
+          <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center ">
+            <a className="text-accent">koreanizefoodapp@email.com</a>
+            <p className="leading-normal my-5">
+              49 Smith St.
+              <br />
+              Saint Cloud, MN 56301
+            </p>
           </div>
         </div>
       </div>
