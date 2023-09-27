e8bc79103e8b2801945a0c9647e818c1a02493ea
Melvin Mangubat <137170998+mrVinzcode@users.noreply.github.com>
2023-09-27 18:57:15 +0800

    change all services logo

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,17 +4,17 @@ import ServicesCard from "./ServicesCard";
 
 const servicesData = [
   {
-    img: "/services__1.png",
+    img: "/services.png",
     title: "Expert Kitchen",
     desc: "Welcome to our professional kitchen, where culinary excellence meets personalized service. Our dedicated team of skilled chefs and staff are committed to delivering an unforgettable dining experience. From crafting exquisite dishes to catering to your dietary preferences, we take pride in every detail. Indulge in a journey of flavors and let us elevate your dining expectations. Your satisfaction is our passion.",
   },
   {
-    img: "/services__2.png",
+    img: "/delivery.png",
     title: "Doorstep Delivery",
     desc: "Introducing our efficient delivery services, designed to bring convenience right to your doorstep. Whether it's a hearty meal, groceries, or essentials, we've got you covered. With a focus on timeliness and safety, our dedicated delivery team ensures your items arrive fresh and in perfect condition. Sit back, relax, and let us take care of the delivery logistics while you enjoy the convenience of staying home. Experience hassle-free deliveries with us today!",
   },
   {
-    img: "/services__4.png",
+    img: "/wifi.png",
     title: "Complimentary WiFi",
     desc: "Stay connected with our complimentary WiFi services! We understand the importance of staying online, whether for work or leisure. Enjoy seamless browsing and streaming with our high-speed WiFi, available to all our valued guests. Share your moments, catch up on emails, or simply browse your favorite sites without any limitations. It's our way of enhancing your experience and ensuring you have all the convenience you need while you're with us.",
   },
